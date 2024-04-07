419e16113aaf4d194bf5d82f42ceab4c377fdd05
passies95 <passies95@gmail.com>
2024-04-07 03:10:50 +0300

    update gdal

--- build.sh
+++ build.sh
@@ -4,8 +4,19 @@ set -o errexit
 
 # Modify this line as needed for your package manager (pip, poetry, etc.)
 # Check system architecture
-architecture=$(uname -m)
-echo "System architecture: $architecture"
+# architecture=$(uname -m)
+# echo "System architecture: $architecture"
+# Insta;=ll gdal
+cd binaries/gdal-3.6.2
+./configure --prefix=$HOME/gdal
+make
+make install
+echo 'export PATH=$HOME/gdal/bin:$PATH' >> ~/.bashrc
+echo 'export LD_LIBRARY_PATH=$HOME/gdal/lib:$LD_LIBRARY_PATH' >> ~/.bashrc
+source ~/.bashrc
+# Verify gdal Installation
+gdalinfo --version
+
 pip install -r requirements.txt
 
 # Convert static asset files
